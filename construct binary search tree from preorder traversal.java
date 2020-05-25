e39da49608977df7b7261d03a5b5b35040d727f3
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2020-05-25 11:12:21 +0530

    Day 24

---
+++ construct binary search tree from preorder traversal.java
@@ -0,0 +1,60 @@
+/*
+class Solution {
+   
+        int index = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder == null || preorder.length == 0){
+            return null;
+        }
+        
+        return bstFromPreorderHelper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    TreeNode bstFromPreorderHelper(int[] preorder, int min, int max){
+        if(index >= preorder.length){
+            return null;
+        }
+        
+        int key = preorder[index];
+        TreeNode node = null;
+        if(key > min && key < max){
+            node = new TreeNode(preorder[index]);
+            index++;
+            node.left = bstFromPreorderHelper(preorder, min, key);
+            node.right = bstFromPreorderHelper(preorder, key, max);
+        }
+        return node;
+    }
+    
+}*/
+
+
+
+class Solution {
+   
+        int index = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder == null || preorder.length == 0){
+            return null;
+        }
+        
+        return bstFromPreorderHelper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    TreeNode bstFromPreorderHelper(int[] preorder, int min, int max){
+        if(index >= preorder.length){
+            return null;
+        }
+        
+        int key = preorder[index];
+        TreeNode node = null;
+        if(key > min && key < max){
+            node = new TreeNode(preorder[index]);
+            index++;
+            node.left = bstFromPreorderHelper(preorder, min, key);
+            node.right = bstFromPreorderHelper(preorder, key, max);
+        }
+        return node;
+    }
+    
+}
